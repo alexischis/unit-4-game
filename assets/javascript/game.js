7330408c1cbbf6123e0ad2d76b96c35473565e70
Alexis Chisolm <chisolmalexis@gmail.com>
2019-09-02 08:18:30 -0400

    added alerts and fixed reset function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,126 +4,112 @@ $(document).ready(function(){
 var crystalValues = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
 
 // set up the numeric variables for the game
-var wins = 0;
-var losses = 0;
-var UserChoice=0;
+var numberWins = 0;
+var numberLosses = 0;
+var userChoice = 0;
 
 // computer can choose any number from 19-120 at random
-var computerGuess =Math.floor(Math.random() *101+19);
+var computerChoice =Math.floor(Math.random() *101+19);
 
+// randomizing the value for each crystal
 var crystalOne = crystalValues[Math.floor(Math.random()* crystalValues.length)];
 var crystalTwo = crystalValues[Math.floor(Math.random()* crystalValues.length)];
 var crystalThree = crystalValues[Math.floor(Math.random()* crystalValues.length)];
 var crystalFour = crystalValues[Math.floor(Math.random()* crystalValues.length)];
 
-var userChoice = [];
-
-
-
-// documenting what letter the computer chooses in the console log
-console.log(numberRange);
+// documenting what numbers the computer chooses and the values of the crystals for each game in the console log
+console.log(computerChoice);
 console.log(userChoice);
 console.log(crystalOne);
 console.log(crystalTwo);
 console.log(crystalThree);
 console.log(crystalFour);
 
-var sum = 0;
-function sum
-for(var i=0; i<userChoice.length;i++){
-  sum += parseInt(userChoice[i]);
-}
-  console.log("total score is" + "" + sum);
+// displaying the computer's value
+$('#random-value').html(computerChoice);
+// displaying wins and losses
+$('#wins').html('Wins:' + "" + numberWins);
+$('#losses').html('Losses:' + "" + numberLosses);
 
-// click event for crystals
+// reset function to allow the computer to choose a different number when the time comes
+function reset() {
+  computerChoice = Math.floor(Math.random() *101+19);
+  console.log('reset', computerChoice);
+  $('#random-value').html(computerChoice);
+  crystalOne = crystalValues[Math.floor(Math.random()* crystalValues.length)];
+  console.log('reset', crystalOne);
+  crystalTwo = crystalValues[Math.floor(Math.random()* crystalValues.length)];
+  console.log('reset', crystalTwo);
+  crystalThree = crystalValues[Math.floor(Math.random()* crystalValues.length)];
+  console.log('reset', crystalThree);
+  crystalFour = crystalValues[Math.floor(Math.random()* crystalValues.length)];
+  console.log('reset', crystalFour);
+  userChoice = 0;
+  $('#total-score').html(userChoice);
+}
+
+// increasing wins
+ function userRight() {
+   alert(" You Did It!");
+   wins ++;
+   $('#wins').html('Wins:' + "" + numberWins);
+   reset();
+ }
+
+ // increasing losses
+ function userWrong() {
+   alert("Too Much!");
+   losses ++;
+   $('#losses').html('Losses:' + "" + numberLosses);
+   reset();
+ }
+
+ // click event for crystals
 $('#crystal1').on("click", function() {
-  crystalChoice = userChoice.push(crystalOne);
-  $('#total-score').html(+userChoice);
-  });
+  userChoice = userChoice += crystalOne;
+  console.log("New userChoice= "+ userChoice);
+  $('#total-score').html(userChoice);
+  if (userChoice === computerChoice) {
+    userRight();
+    }
+  else if (userChoice > computerChoice) {
+    userWrong();
+  }
+})
 
 $('#crystal2').on("click", function() {
-  crystalChoice = userChoice.push(crystalTwo);
-  $('#total-score').html(+userChoice);
-  });
+  userChoice = userChoice + crystalTwo;
+  console.log("New userChoice= "+ userChoice);
+  $('#total-score').html(userChoice);
+  if (userChoice === computerChoice) {
+    userRight();
+    }
+  else if (userChoice > computerChoice) {
+    userWrong();
+  }
+})
 
 $('#crystal3').on("click", function() {
-  crystalChoice = userChoice.push(crystalThree);
-  $('#total-score').html(+userChoice);
-  });
-      
+  userChoice = userChoice + crystalThree;
+  console.log("New userChoice= "+ userChoice);
+  $('#total-score').html(userChoice);
+  if (userChoice === computerChoice) {
+    userRight();
+    }
+  else if (userChoice > computerChoice) {
+    userWrong();
+  }
+})
+
 $('#crystal4').on("click", function() {
-  crystalChoice = userChoice.push(crystalFour);
-  $('#total-score').html(+userChoice);
-  });
-
- 
-
-
-
-  // $('#total-score').html(this);
-
-// reset function to allow the computer to choose a different letter when the time comes
-function reset() {
-    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-    console.log('reset', computerGuess);
-    crystalOne = crystalValues[Math.floor(Math.random()* crystalValues.length)];
-    console.log('reset', crystalOne);
-    crystalTwo = crystalValues[Math.floor(Math.random()* crystalValues.length)];
-    console.log('reset', crystalTwo);
-    crystalThree = crystalValues[Math.floor(Math.random()* crystalValues.length)];
-    console.log('reset', crystalThree);
-    crystalFour = crystalValues[Math.floor(Math.random()* crystalValues.length)];
-    console.log('reset', crystalFour);
-}
-
-// // user can select a letter using their keyboard, the game starts when a key is pushed
-// document.onkeyup = function (event) {
-//     var userGuess = event.key;
-//     var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
-
-// // setting up possible key choices for the user, the game will not accept a number
-//     if (options.indexOf(userGuess) > -1) {
-
-// // if the user selects the same letter as the computer then the user earns a point
-// // guesses and choices are reset
-//         if (userGuess === computerGuess) {
-//             wins++;
-//             numGuesses = 9;
-//             guessChoices = [];
-            
-//         }
-
-// // if the user choices a letter that is not the same as the computer then the number of guesses goes down
-// // and the letter chosen will appear on the screen
-//         if (userGuess != computerGuess) {
-//             numGuesses--;
-//             guessChoices.push(userGuess);
-//         }
-
-// // if the number of guesses left goes down to zero then the users losses increase by 1
-// //  the guess and choices are reset
-//         if (numGuesses === 0) {
-//             numGuesses = 9;
-//             losses++;
-//             guessChoices = [];
-//         }
-
-// // when the guesses are reset to nine the computers choice is also reset
-//         if (numGuesses === 9) {
-//             reset();
-//         }
-
-// // linking in the html text and adding the variables
-//         var html =
-//             "<h1> Psychic Game </h1>" +
-//             "<p>Guess what letter I'm thinking of!</p>" +
-//             "<p>Press any letter to see if you're right.</p>" +
-//             "<p>Wins: " + wins + "</p>" +
-//             "<p>Losses: " + losses + "</p>" +
-//             "<p>Guesses Left: " + numGuesses + "</p>" +
-//             "<p>Your Guesses so far: " + guessChoices.join(", ") + "</p>";
-
-//         document.querySelector("#game").innerHTML = html;
-
-//     }
-// };
+  userChoice = userChoice + crystalTFour;
+  console.log("New userChoice= "+ userChoice);
+  $('#total-score').html(userChoice);
+  if (userChoice === computerChoice) {
+    userRight();
+    }
+  else if (userChoice > computerChoice) {
+    userWrong();
+  }
+});
+});
